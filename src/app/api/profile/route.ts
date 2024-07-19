b91c6288bd7b12140b3f54910daccc3faa6a0ebf
holymaiden <haikalwatampone3@gmail.com>
2024-07-19 15:34:05 +0800

    feat: profile

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,6 +1,28 @@
 import prisma from '@/config/prisma';
 import { NextRequest } from 'next/server';
 
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+
+  const email = searchParams.get('email') || '';
+
+  const data = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!data) {
+    return new Response(JSON.stringify({ message: 'Data not found' }), {
+      status: 404,
+    });
+  }
+
+  return new Response(JSON.stringify({ data: data }), {
+    status: 200,
+  });
+}
+
 export async function PUT(request: NextRequest) {
   const body = await request.json();
 
